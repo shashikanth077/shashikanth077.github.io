d3476f8b5d01c324a9b00139483991daadbd718f
shashikanth077 <shashikanth033@gmail.com>
2026-07-25 23:03:43 +0530

    Update favicon to new image and adjust layout references

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -29,8 +29,8 @@ const Layout = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/img/logo/logo.png" type="image/png" />
-        <link rel="shortcut icon" href="/img/logo/logo.png" />
+        <link rel="icon" href="/img/logo/fevicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/img/logo/fevicon.png" />
       </Head>
       <Preloader />
       <VideoPopup />
